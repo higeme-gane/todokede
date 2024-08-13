c73a4ea45458ad72cf401693c8fcb75983b20aef
higeme-gane <rr34vv@gmail.com>
2024-08-13 15:58:17 +0900

    Update map_koseikyoku_raw.R

--- map_koseikyoku_raw.R
+++ map_koseikyoku_raw.R
@@ -2,258 +2,135 @@ library(conflicted)
 library(tidyverse)
 library(rvest)
 library(httr)
+library(fs)
 library(openxlsx)
 library(zipangu)
 
-#東北、関東信越、近畿、中国、四国
+# ベースURLを定義する
+base_url <- c("https://kouseikyoku.mhlw.go.jp")
+pattern <- "https://kouseikyoku.mhlw.go.jp/([^/]+)/"
+
+# 各行のヘッダーを個別に抽出してデータフレーム化するモジュール
+extract_headers <- function(header_list) {
+  tibble(
+    last_modified = header_list$`last-modified`,
+    etag = header_list$etag,
+    content_length = header_list$`content-length`,
+    accept_ranges = header_list$`accept-ranges`,
+    content_type = header_list$`content-type`,
+    date = header_list$date
+  )
+}
+
 df_kouseikyoku <- dplyr::tibble(
   url = c(
+    "https://kouseikyoku.mhlw.go.jp/hokkaido/gyomu/gyomu/hoken_kikan/todokede_juri_ichiran.html",
     "https://kouseikyoku.mhlw.go.jp/tohoku/gyomu/gyomu/hoken_kikan/documents/201805koushin.html",
     "https://kouseikyoku.mhlw.go.jp/kantoshinetsu/chousa/kijyun.html",
-    "https://kouseikyoku.mhlw.go.jp/shikoku/gyomu/gyomu/hoken_kikan/shitei/index.html",
+    "https://kouseikyoku.mhlw.go.jp/tokaihokuriku/newpage_00349.html",
     "https://kouseikyoku.mhlw.go.jp/kinki/gyomu/gyomu/hoken_kikan/shitei_jokyo_00004.html",
-    "https://kouseikyoku.mhlw.go.jp/chugokushikoku/chousaka/shisetsukijunjuri.html"),
-  contains_jp = c("Excel（ZIP）", "医科（ZIP）", "（ZIP）", "（ZIP）", "各県全体（ZIP）")) |> 
-    mutate(a_con = str_c("a:contains('", contains_jp, "')"))
-  
-map(1:nrow(df_kouseikyoku), ~{
-# ターゲットURL
-url <- df_kouseikyoku$url[.x]
-a_contains <- df_kouseikyoku$a_con[.x]
-# ウェブページを読み込み、必要なリンクを抽出する
-web_page <- read_html(url)
-links <- web_page %>% html_nodes(a_contains) %>% html_attr("href")
+    "https://kouseikyoku.mhlw.go.jp/chugokushikoku/chousaka/shisetsukijunjuri.html",
+    "https://kouseikyoku.mhlw.go.jp/shikoku/gyomu/gyomu/hoken_kikan/shitei/index.html",
+    "https://kouseikyoku.mhlw.go.jp/kyushu/gyomu/gyomu/hoken_kikan/index_00007.html"),
+  what_num = c(rep.int(1, times = 6), 4, c("1:8"))) |>
+  mutate(regions = str_extract(url, pattern)) |>
+  mutate(regions = str_remove(regions, base_url)) |>
+  mutate(regions = str_remove_all(regions, "^/|/$")) |> 
+  mutate(extension = if_else(regions == "hokkaido",
+                             true =  c(".xlsx"),
+                             false =  c(".zip")))
 
-# ベースURLを定義する
-base_url <- "https://kouseikyoku.mhlw.go.jp"
+df_links <- map(1:nrow(df_kouseikyoku), ~{
+  links_url <- read_html(df_kouseikyoku$url[.x]) |> 
+    html_nodes("a") |> 
+    html_attr("href")
+  slice_num <- eval(parse(text = df_kouseikyoku$what_num[.x]))
+  dplyr::tibble(each_link = links_url) |> 
+    mutate(extension = str_extract(each_link, "\\.[a-zA-Z0-9]+$")) |> 
+    dplyr::filter(extension == df_kouseikyoku$extension[.x]) |> 
+    slice(slice_num) |> 
+    mutate(full_link = str_c(base_url, each_link))
+}) |> list_rbind()
 
-# リンクの完全なURLを生成する
-full_links <- paste0(base_url, links)
-
-# 各リンク先ファイルのサイズを取得する
-get_file_size <- function(url) {
-  response <- HEAD(url)
-  as.numeric(headers(response)["content-length"])
-}
-
-file_sizes <- sapply(full_links, get_file_size)
-
-# 最大サイズのファイルのリンクを取得する
-max_index <- which.max(file_sizes)
-max_file_url <- full_links[max_index]
+df_headers_work <- map(df_links$full_link, function(fn_work){
+  res <- HEAD(fn_work)
+  dplyr::tibble(
+    url = fn_work,
+    headers = list(headers(res))
+  )
+}) |> list_rbind()
+df_headers <- df_headers_work |> 
+  mutate(headers = map(headers, extract_headers)) |> 
+  unnest(headers) |> 
+  mutate(content_length = as.integer(content_length)) |> 
+  mutate(across(c(last_modified, date), dmy_hms))
 
 # ダウンロードディレクトリを設定する
 download_dir <- "downloads"
 if (!dir.exists(download_dir)) {
   dir.create(download_dir)
 }
+# 該当ファイルをダウンロードする
+map(df_headers$url, ~{
+  file_name <- basename(.x)
+  file_path <- file.path(download_dir, file_name)
+  download.file(.x, file_path, mode = "wb")
+})
+#downloads_dirにある.zipファイル名を取得する。
+df_downloads_dir <- dplyr::tibble(file_name = list.files(download_dir)) |> 
+  mutate(extension = str_extract(file_name, "\\.[a-zA-Z0-9]+$")) |> 
+  dplyr::filter(extension == ".zip")
 
-# 最大サイズのファイルをダウンロードする
-zip_filename <- basename(max_file_url)
-zip_filepath <- file.path(download_dir, zip_filename)
-download.file(max_file_url, zip_filepath)
-
-# ダウンロードされたファイルを解凍する
-unzip_dir <- tempdir()
-
-# システムコマンドを使って解凍する
-zip_command <- paste("unzip", shQuote(zip_filepath), "-d", shQuote(unzip_dir))
-system(zip_command)
-
-# 元のディレクトリに保存するためのパスを確認する
-original_dir <- dirname(download_dir)
-
-# 解凍されたフォルダ内のエクセルファイルを検索し、元のディレクトリにコピーする
-unzip_subdirs <- list.dirs(unzip_dir, recursive = TRUE)
-
-for (subdir in unzip_subdirs) {
-  excel_files <- list.files(subdir, pattern = "\\.xlsx$", full.names = TRUE)
-  if (length(excel_files) > 0) {
-    file.copy(excel_files, original_dir, overwrite = TRUE)
-  }
+# 元のカレントディレクトリを保存
+original_dir <- getwd()
+# "downloads"ディレクトリに移動する
+setwd(download_dir)
+# downloadディレクトリの絶対パスを取得
+download_dir <- getwd()
+# zipディレクトリを設定し、zipファイルのみzipディレクトリに移動
+zip_dir <- "zips"
+if (!dir.exists(zip_dir)) {
+  dir.create(zip_dir)
 }
-
-#downloadディレクトリを削除する
-unlink(download_dir, recursive = TRUE)
+file_move(df_downloads_dir$file_name, zip_dir)
+# zipディレクトリの絶対パスを取得
+setwd(zip_dir)
+zip_dir <- getwd()
+df_downloads_dir <- mutate(.data = df_downloads_dir,
+                           zip_file_name = str_c(zip_dir, "/", file_name))
+# システムコマンドを使って解凍する
+map(df_downloads_dir$zip_file_name, ~{
+  zip_command <- paste("unzip", shQuote(.x), "-d", shQuote(download_dir))
+  system(zip_command)
 })
 
-#北海道
-url <- "https://kouseikyoku.mhlw.go.jp/hokkaido/gyomu/gyomu/hoken_kikan/todokede_juri_ichiran.html"
+setwd(download_dir)
+sub_dirs <- dir_ls(download_dir, recurse = FALSE, type = "directory")
+# 各ディレクトリ内の.xlsxファイルを探して移動
+map(sub_dirs, ~{
+  df_dir_work <- dplyr::tibble(full_path = dir_ls(.x)) |>
+  mutate(extension = str_extract(full_path, "\\.[a-zA-Z0-9]+$")) |> 
+  dplyr::filter(extension == ".xlsx")
+file_move(df_dir_work$full_path, download_dir)
+dir_delete(.x)
+})
+#九州厚生局のファイルには歯科、薬局の施設基準も含まれているため除外
+df_shika_yakkyoku <- dplyr::tibble(file_name =  list.files(download_dir)) |> 
+  dplyr::filter(str_detect(file_name, c("shika")) |
+                str_detect(file_name, c("yakkyoku"))) |> 
+  mutate(full_link = str_c(download_dir, "/", file_name))
+file.remove(df_shika_yakkyoku$full_link)
 
-# ウェブページのHTMLを読み込む
-webpage <- read_html(url)
-
-# エクセルファイルへのリンクを取得
-links <- webpage %>%
-  html_nodes("a") %>%
-  html_attr("href")
-
-# エクセルファイルのリンクを特定
-excel_links <- links[grepl("\\.xlsx$", links)]
-
-# エクセルファイルが存在するか確認
-if (length(excel_links) == 0) {
-  stop("エクセルファイルのリンクが見つかりませんでした。")
-}
-
-# ダウンロードしたファイル名を格納するリスト
-downloaded_files <- c()
-
-# ベースURLとファイルリンクを結合し、すべてのエクセルファイルをダウンロード
-for (link in excel_links) {
-  # 絶対URLに変換
-  download_link <- paste0("https://kouseikyoku.mhlw.go.jp", link)
-  
-  # ファイル名を抽出
-  file_name <- basename(link)
-  
-  # ファイルをダウンロードして保存
-  GET(download_link, write_disk(file_name, overwrite = TRUE))
-  cat(file_name, "のダウンロードが完了しました。\n")
-  
-  # ダウンロードしたファイル名をリストに追加
-  downloaded_files <- c(downloaded_files, file_name)
-}
-
-# ファイルサイズを取得して最大のファイルを特定
-file_sizes <- sapply(downloaded_files, file.size)
-largest_file <- downloaded_files[which.max(file_sizes)]
-
-cat("最大ファイルは:", largest_file, "です。\n")
-
-# 他のファイルを削除
-files_to_delete <- setdiff(downloaded_files, largest_file)
-sapply(files_to_delete, file.remove)
-
-#東海
-# ダウンロード対象ページのURL
-url <- "https://kouseikyoku.mhlw.go.jp/tokaihokuriku/newpage_00349.html"
-
-# ウェブページを読み込み
-webpage <- read_html(url)
-
-# 対象リンクを特定
-links <- webpage %>%
-  html_nodes("a") %>%
-  .[str_detect(html_text(.), "^届出受理医療機関名簿（医科）")] %>%
-  html_attr("href")
-
-# 最初のリンクを取得
-first_link <- links[1]
-
-# 完全なURLを作成
-file_url <- paste0("https://kouseikyoku.mhlw.go.jp", first_link)
-
-# ダウンロードディレクトリを設定する
-download_dir <- "downloads"
-if (!dir.exists(download_dir)) {
-  dir.create(download_dir)
-}
-
-# 該当ファイルをダウンロードする
-zip_filename <- basename(file_url)
-zip_filepath <- file.path(download_dir, zip_filename)
-download.file(file_url, zip_filepath)
-
-# ダウンロードされたファイルを解凍する
-unzip_dir <- tempdir()
-
-# システムコマンドを使って解凍する
-zip_command <- paste("unzip", shQuote(zip_filepath), "-d", shQuote(unzip_dir))
-system(zip_command)
-
-# 元のディレクトリに保存するためのパスを確認する
-original_dir <- dirname(download_dir)
-
-# 解凍されたフォルダ内のエクセルファイルを検索し、元のディレクトリにコピーする
-unzip_subdirs <- list.dirs(unzip_dir, recursive = TRUE)
-
-for (subdir in unzip_subdirs) {
-  excel_files <- list.files(subdir, pattern = "\\.xls(x)?$", full.names = TRUE)
-  if (length(excel_files) > 0) {
-    file.copy(excel_files, original_dir, overwrite = TRUE)
-  }
-}
-
-#downloadディレクトリを削除する
-unlink(download_dir, recursive = TRUE)
-
-#九州
-# ターゲットURL
-url <- "https://kouseikyoku.mhlw.go.jp/kyushu/gyomu/gyomu/hoken_kikan/index_00007.html"
-
-# ウェブページを読み込み、必要なリンクを抽出する
-web_page <- read_html(url)
-links <- web_page %>% html_nodes("a:contains('エクセルデータ（ZIP）')") %>% html_attr("href")
-
-# 最新ファイル判定のための正規表現を作成
-latest_prefix <- max(gsub("^.*/([^_]+_[0-9]+)_.*$", "\\1", links))
-
-# ダウンロードディレクトリを設定する
-download_dir <- "downloads"
-if (!dir.exists(download_dir)) {
-  dir.create(download_dir)
-}
-
-# ダウンロードディレクトリを設定する
-download_dir <- "downloads"
-if (!dir.exists(download_dir)) {
-  dir.create(download_dir)
-}
-
-# 最新のリンクのみを抽出してダウンロード
-latest_links <- links[grepl(paste0(latest_prefix), links)]
-  
-for (link in latest_links) {
-  zip_url <- paste0("https://kouseikyoku.mhlw.go.jp", link)
-  zip_filename <- basename(zip_url)
-  zip_filepath <- file.path(download_dir, zip_filename)
-  download.file(zip_url, zip_filepath)
-}
-
-list.files("./downloads")
-
-# 解凍用ディレクトリを設定する
-unzip_dir <- "unzipped"
-if (!dir.exists(unzip_dir)) {
-  dir.create(unzip_dir)
-}
-
-# 解凍したファイルをフィルタリングして移動する
-target_dir <- "target_files"
-if (!dir.exists(target_dir)) {
-  dir.create(target_dir)
-}
-
-# 元のディレクトリに移動するための作業ディレクトリを取得
-original_dir <- getwd()
-
-# ダウンロードしたZIPファイルを解凍し、条件に合うファイルを移動する
-zip_files <- list.files(download_dir, pattern = "\\.zip$", full.names = TRUE)
-for (zip_file in zip_files) {
-  unzip(zip_file, exdir = unzip_dir)
-  files <- list.files(unzip_dir,
-     pattern = str_c(latest_prefix, ".*_ika_.*\\.xlsx$"), full.names = TRUE)
-  if (length(files) > 0) {
-    file.copy(files, original_dir, overwrite = TRUE) 
-  }
-  # 解凍したファイルを削除
-  unlink(list.files(unzip_dir, full.names = TRUE, recursive = TRUE))
-}
-
-# 使用したディレクトリを削除する
-unlink(download_dir, recursive = TRUE)
-unlink(unzip_dir, recursive = TRUE)
-unlink(target_dir, recursive = TRUE)
+# 元のカレントディレクトリに戻る
+setwd(original_dir)
 
 #ダウンロードしたファイルを読み込みデータ処理
-df_file <- dplyr::tibble(name = list.files()) |> 
-  # mutate(path = str_c("./todokede/", name)) |> 
-  mutate(extension = str_sub(name, -5, -1)) |> 
+df_file <- dplyr::tibble(full_path = dir_ls(download_dir)) |> 
+  mutate(extension = str_extract(full_path, "\\.[a-zA-Z0-9]+$")) |> 
   dplyr::filter(extension == ".xlsx")
-
+read.xlsx(df_file$full_path[1])
 #47都道府県のエクセルデータを結合。
-df_raw <- map(df_file$name, function(fn_xlsx_file) {
+df_raw <- map(df_file$full_path, function(fn_xlsx_file) {
   sheets <- getSheetNames(fn_xlsx_file)
   map(sheets, function(fn_sheet) {
     read.xlsx(fn_xlsx_file, startRow = 4, sheet = fn_sheet) |> 
@@ -296,9 +173,6 @@ piv_pref_latest <- distinct(.data = df_raw, pref_code, pref_name, start_date,
 #日本語の列名に戻す。
 colnames(df_raw) <- jp_colnames
 
-#各都道府県のエクセルファイルの削除。必要に応じてコメントアウトを。
-file.remove(df_file$name)
-
 #ファイル出力(.rdsは約15MB、.csvは約300MB、.xlsxだと約80MBのファイルが出力される。)
 #write.xlsx()は若干時間がかかる。
 saveRDS(df_raw, str_c("rawfile_", Sys.Date(),".rds"))
@@ -307,3 +181,6 @@ saveRDS(df_raw, str_c("rawfile_", Sys.Date(),".rds"))
 
 #都道府県ごとの更新日を.csv出力
 write_excel_csv(piv_pref_latest, str_c("update_date_pref_", Sys.Date(), ".csv"))
+
+#headers情報の.csv出力
+write_csv(df_headers, str_c("headers", Sys.Date(), ".csv"))
